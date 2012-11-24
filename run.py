d7f133d279e979546cdad1195a7e455fae18fd87
ILJICH <iljich@iljich.name>
2012-11-24 07:46:23 +0400

    Introducing shell arguments

--- run.py
+++ run.py
@@ -1,26 +1,36 @@
 # -*- coding: utf-8 -*-
+from argparse import ArgumentParser
 from signal import SIGTERM, SIGHUP
 from daemon import daemon
 from lockfile import FileLock
-import sys
 from pybd.processor import Processor
 from pybd.utils import FileLikeLogger
+from os import path
 
 __author__ = 'iljich'
 
+parser = ArgumentParser()
+parser.add_argument("-c", "--config", help="path to config file")
 
-context = daemon.DaemonContext(
-    pidfile=FileLock('/var/run/pybd.pid'),
-    uid = 0,
-    stderr=FileLikeLogger("ERROR"),
-    stdout=FileLikeLogger("INFO"),
-)
+args = parser.parse_args()
 
-context.signal_map = {
-    SIGTERM: lambda s,f: Processor().exit(),
-    SIGHUP: lambda s,f: Processor().exit()
-}
+if args.config:
+    context = daemon.DaemonContext(
+        pidfile=FileLock('/var/run/pybd.pid'),
+        uid = 0,
+        stderr=FileLikeLogger("ERROR"),
+        stdout=FileLikeLogger("INFO"),
+    )
 
-with context:
-    p = Processor("/home/iljich/PycharmProjects/pybd/pybd.conf")
-    p.run()
+    context.signal_map = {
+        SIGTERM: lambda s,f: Processor().exit(),
+        SIGHUP: lambda s,f: Processor().exit()
+    }
+
+    config_path = path.abspath(args.config)
+
+    with context:
+        p = Processor(config_path)
+        p.run()
+
+parser.print_help()
